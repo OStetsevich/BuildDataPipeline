7ffc0ba22f7e73ee2e754dedf9153a5462dbc5f9
OStetsevich <stetsevicholga@gmail.com>
2026-05-03 17:47:25 +0200

    Added new funcyion to video_stats.py file

--- video_stat.py
+++ video_stat.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env")
 
 api_key = os.getenv("api_key")
 channel_handle = "MrBeast&key"
+max_results = 50
 
 url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}={api_key}"
 
@@ -23,5 +24,36 @@ def get_playlist_id():
     except requests.exceptions.RequestException as error:
         raise error
     
+def get_playlists_id(play_list_id):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={play_list_id}&key={api_key}"
+
+    try:
+        while True:
+          
+          url = base_url
+
+          if pageToken:
+              url +=f"&pageToken={pageToken}"
+
+          response = requests.get(url)
+          response.raise_for_status()
+          data = response.json()
+
+          for item in data.get("items", []):
+              video_id = item["contentDetails"]["videoId"]
+              video_ids.append(video_id)
+
+          pageToken = data.get("nextPageToken")
+
+          if not pageToken:
+              break
+        return video_ids
+
+    except requests.exceptions.RequestException as error:
+        raise error
+
 if __name__ == "__main__":
-    get_playlist_id()
+    play_list_id = get_playlist_id()
+    get_playlists_id(play_list_id)
